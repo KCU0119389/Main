d1d57fdfd2d5a68d5e03245c45eb8ff1e70b2ac9
KCU0119389 <zoyakhankle@gmail.com>
2025-06-05 07:08:50 +0530

    main

--- Student.java
+++ Student.java
@@ -1,5 +1,7 @@
 package Main;
+
 public class Student {
+   
 public static void main(String[] args){
     String name="zoya";
     int Birthyear=2005;
@@ -10,3 +12,6 @@ public static void main(String[] args){
     System.out.println("Age:"+age);
 }
 }
+
+    
+
