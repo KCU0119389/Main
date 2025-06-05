84825a2d66b53215222e3ac2db3f992f575ff725
KCU0119389 <zoyakhankle@gmail.com>
2025-06-05 07:00:03 +0530

    save student java

---
+++ Student.java
@@ -0,0 +1,12 @@
+package Main;
+public class Student {
+public static void main(String[] args){
+    String name="zoya";
+    int Birthyear=2005;
+    int currentyear=2025;
+    int age=currentyear-Birthyear;
+
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+}
+}
